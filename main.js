a67f105e441ad75414c9606861abf36a0c1b888a
Pedro Gonzalez <pedro.gch01@gmail.com>
2022-11-22 00:28:29 -0300

    tp2 idm

--- main.js
+++
@@ -1,7 +0,0 @@
-let menutoggle = document.querySelector('.toggle');
-let menu = document.querySelector('.menu');
-menutoggle.onclick =  function(){
-  menutoggle.classList.toggle('active');
-  menu.classList.toggle('mostrar');
-  
-}
